0d04894a30da2fa437b504d5be99fca87438c043
Thomas Domingues <thomas_domingues@yahoo.fr>
2018-03-09 17:24:07 +0100

    Initial Commit

---
+++ Muskle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muskle
+//
+//  Created by Thomas DOMINGUES on 09/03/2018.
+//  Copyright © 2018 Thomas Domingues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
